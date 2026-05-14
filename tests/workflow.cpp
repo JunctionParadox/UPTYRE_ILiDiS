1178ef08d1eb263733fddd0ee5a2b5e7ee67486c
JunctionParadox <Novasigma89@gmail.com>
2026-05-14 14:19:56 +0200

    Added test variant of main for github workflow

---
+++ tests/workflow.cpp
@@ -0,0 +1,47 @@
+#ifndef WORKFLOW
+
+#include <iostream>
+#include "gui.h" //Gui.cpp header file
+#include "winctrl.h" //Wincrtl.cpp header file
+#include "workflow.h"
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx11.h"
+
+
+
+//This is the same as the main() function
+//Albeit with one modification
+//There is no program lifecycle
+//Every function is tested only once
+static int WorkflowTest() {
+    WndController wndcontrol = WndController();
+    ImguiController imguicontrol = ImguiController();
+
+    wndcontrol.WindowInit(imguicontrol.GetDpi());
+
+    if (!imguicontrol.CreateDeviceD3D(wndcontrol.GetWndHWND()))
+    {
+        imguicontrol.CleanupDeviceD3D();
+        wndcontrol.WindowUnregister();
+        return 1;
+    }
+
+    imguicontrol.InitGui(wndcontrol.GetWndHWND());
+
+    wndcontrol.operational = true;
+
+    //Unlike the normal program
+    //There cannot be a loop
+    //As github actions will run it indefinitely
+    //Meaning it cannot declare a succes status
+    wndcontrol.MessageLoop();
+    imguicontrol.CheckSwapChain();
+    imguicontrol.ResizeGui();
+    imguicontrol.RenderGui();
+    
+    imguicontrol.DestroyGui(wndcontrol.GetWndHWND());
+    wndcontrol.WindowUnregister();
+}
+
+#endif
